0a9c58b4a04b8f324793e67abdf14ffcaeecd3e5
dev <dev@dhcppc1.dev>
2024-10-23 17:08:11 +0430

    ffed is addec

--- Frontend/src/Components/home.jsx
+++ Frontend/src/Components/home.jsx
@@ -1,7 +1,18 @@
 function f (){
+    function ffed(){
+        return(
+        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur possimus quaerat maxime eveniet minima corporis beatae, officia deserunt numquam ea reprehenderit ducimus fugiat neque sed officiis magni nesciunt, fugit voluptates!</div>
+    )
+}
+
+export default ffed
+
     return(
         <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur possimus quaerat maxime eveniet minima corporis beatae, officia deserunt numquam ea reprehenderit ducimus fugiat neque sed officiis magni nesciunt, fugit voluptates!</div>
     )
 }
 
 export default f
+
+
+
