54b9fe3426cd5cb103dad38cb138ecd81f4e2a56
Anwar <anwarmohammadi1390@gmail.com>
2024-10-23 15:23:25 +0430

    First Front Commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere
+        doloribus iste quasi in dolor quisquam assumenda cupiditate sint. Atque
+        a veniam nostrum neque, culpa ut quod sit numquam cumque architecto.
+      </p>
+    </>
+  );
+}
+
+export default App;
